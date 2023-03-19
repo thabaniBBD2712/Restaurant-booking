d91e642debc2697c81a47d267f4f8c2c69d6efdf
Lisa Nolte <lisan@bbd.co.za>
2023-03-19 16:21:55 +0200

    Added spring project setup

---
+++ restaurantBooking/src/test/java/com/restaurantBooking/RestaurantBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
